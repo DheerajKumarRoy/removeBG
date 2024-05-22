dd6990789bbec81c67385aed1cbfb3c0c1a4eb13
DheerajKumarRoy <131980711+DheerajKumarRoy@users.noreply.github.com>
2024-05-23 05:23:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -65,5 +65,5 @@ def download_file(filename):
 
     return send_file(file_path, as_attachment=True)
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=True,host='0.0.0.0')
